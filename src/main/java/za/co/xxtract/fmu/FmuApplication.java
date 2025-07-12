3789547f5f21f20f34be48aa4c4f52d92463f649
Musa Baloyi <musambaloyi@gmail.com>
2025-07-12 17:31:49 +0200

    initial commit

---
+++ src/main/java/za/co/xxtract/fmu/FmuApplication.java
@@ -0,0 +1,13 @@
+package za.co.xxtract.fmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/xxtract/fmu/FmuApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.xxtract.fmu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
